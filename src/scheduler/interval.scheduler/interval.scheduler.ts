fc2c853632c1c8cc544c9d4a3b5b5e5ea7ba0d72
Hugo Herrador <hugo.herrador@inetum.com>
2024-04-11 11:37:10 +0200

    implements onApplicationhsutDown

--- src/scheduler/interval.scheduler/interval.scheduler.ts
+++ src/scheduler/interval.scheduler/interval.scheduler.ts
@@ -1,16 +1,28 @@
-import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import {
+  Injectable,
+  OnApplicationBootstrap,
+  OnApplicationShutdown,
+} from '@nestjs/common';
 import { DiscoveryService, Reflector, MetadataScanner } from '@nestjs/core';
 import { wrap } from 'module';
 import { INTERVAL_HOST_KEY } from '../decoratos/interval-host.decorator';
 import { INTERVAL_KEY } from '../decoratos/interval.decorator';
 
 @Injectable()
-export class IntervalScheduler implements OnApplicationBootstrap {
+export class IntervalScheduler
+  implements OnApplicationBootstrap, OnApplicationShutdown
+{
+  private readonly intervals: NodeJS.Timer[] = [];
   constructor(
     private readonly discoveryService: DiscoveryService,
     private readonly reflector: Reflector,
     private readonly metadataScanner: MetadataScanner,
   ) {}
+  
+  onApplicationShutdown(signal?: string) {
+    this.intervals.forEach((interval) =>  clearInterval(interval));
+  }
+
   onApplicationBootstrap() {
     const providers = this.discoveryService.getProviders();
     providers.forEach((wrapper) => {
@@ -30,7 +42,8 @@ export class IntervalScheduler implements OnApplicationBootstrap {
         if (interval === undefined) {
           return;
         }
-        setInterval(() => instance[methodKey](), interval);
+        const intervalRef = setInterval(() => instance[methodKey](), interval);
+        this.intervals.push(intervalRef);
       });
     });
   }
